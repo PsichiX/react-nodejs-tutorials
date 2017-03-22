546259c86828481b767f84c360c4ef62d6a9425c
PsichiX <psichix@gmail.com>
2017-03-22 23:00:05 +0100

    tutorial part 1

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var webpack = require('webpack');
+var path = require('path');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+var BUILD_DIR = path.resolve(__dirname, 'bin');
+var APP_DIR = path.resolve(__dirname, 'src');
+
+var config = {
+  entry: APP_DIR + '/index.js',
+  module: {
+    loaders: [
+      { test : /\.jsx?$/, include : APP_DIR, loader : 'babel-loader' }
+    ]
+  },
+  output: {
+    path: BUILD_DIR,
+    filename: 'app.js'
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: 'static/index.html' }
+    ])
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, './src'),
+  },
+};
+
+module.exports = config;
